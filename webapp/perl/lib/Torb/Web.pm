d264531a5f5c48d852fa9936f177c1e449eb7287
sonots <sonots@gmail.com>
2018-08-21 22:45:51 +0900

    Add 借金ぐらしのアリエナイッティ

--- webapp/perl/lib/Torb/Web.pm
+++ webapp/perl/lib/Torb/Web.pm
@@ -84,7 +84,7 @@ get '/initialize' => sub {
     my $txn = $self->dbh->txn_scope();
     $self->dbh->query('DELETE FROM users WHERE id > 1000');
     $self->dbh->query('DELETE FROM reservations WHERE id > 1000');
-    $self->dbh->query('DELETE FROM events WHERE id > 3');
+    $self->dbh->query('DELETE FROM events WHERE id > 4');
     $self->dbh->query('UPDATE events SET public_fg = 0, closed_fg = 1');
     $self->dbh->query('UPDATE events SET public_fg = 1, closed_fg = 0 WHERE id = 1');
     $self->dbh->query('UPDATE events SET public_fg = 0, closed_fg = 0 WHERE id = 2');
