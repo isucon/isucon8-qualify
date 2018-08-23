431268f95e1ea060754c5716e9c8ed683f11cd69
karupanerura <karupa@cpan.org>
2018-08-23 17:58:04 +0900

    diet inital code

--- webapp/perl/lib/Torb/Web.pm
+++ webapp/perl/lib/Torb/Web.pm
@@ -16,13 +16,7 @@ filter login_required => sub {
         my ($self, $c) = @_;
 
         my $user = $self->get_login_user($c);
-        unless ($user) {
-            my $res = $c->render_json({
-                error => 'login_required',
-            });
-            $res->status(401);
-            return $res;
-        }
+        return $self->res_error($c, login_required => 401) unless $user;
 
         $app->($self, $c);
     };
@@ -102,11 +96,12 @@ post '/api/users' => [qw/allow_json_request/] => sub {
 
     my ($user_id, $error);
 
+    my $res;
     my $txn = $self->dbh->txn_scope();
     eval {
         my $duplicated = $self->dbh->select_one('SELECT * FROM users WHERE login_name = ?', $login_name);
         if ($duplicated) {
-            $error = 'duplicated';
+            $res = $self->res_error($c, duplicated => 409);
             $txn->rollback();
             return;
         }
@@ -116,39 +111,16 @@ post '/api/users' => [qw/allow_json_request/] => sub {
         $txn->commit();
     };
     if ($@) {
-        $txn->rollback();
         warn "rollback by: $@";
-        $error = 'unknown';
+        $txn->rollback();
+        $res = $self->res_error($c);
     }
+    return $res if $res;
 
-    if ($error) {
-        my $res = $c->render_json({
-            error => $error,
-        });
-        $res->status(500);
-        $res->status(409) if $error eq 'duplicated';
-        return $res;
-    }
-
-    my $user = $self->get_user($user_id);
-    delete $user->{recent_reservations};
-    delete $user->{recent_events};
-
-    my $res = $c->render_json($user);
+    $res = $c->render_json({ id => 0+$user_id, nickname => $nickname });
     $res->status(201);
     return $res;
 };
-
-sub get_user {
-    my ($self, $user_id) = @_;
-    my $user = $self->dbh->select_row('SELECT * FROM users WHERE id = ?', $user_id);
-    return unless $user;
-
-    # sanitize fields
-    delete $user->{login_name};
-    delete $user->{pass_hash};
-    return $user;
-}
 
 sub get_login_user {
     my ($self, $c) = @_;
@@ -156,23 +128,15 @@ sub get_login_user {
     my $session = Plack::Session->new($c->env);
     my $user_id = $session->get('user_id');
     return unless $user_id;
-
-    my $user = $self->get_user($user_id);
-    delete $user->{recent_reservations};
-    delete $user->{recent_events};
-    return $user;
+    return $self->dbh->select_row('SELECT id, nickname FROM users WHERE id = ?', $user_id);
 }
 
 get '/api/users/{id}' => [qw/login_required/] => sub {
     my ($self, $c) = @_;
 
-    my $user = $self->get_user($c->args->{id});
+    my $user = $self->dbh->select_row('SELECT id, nickname FROM users WHERE id = ?', $c->args->{id});
     if ($user->{id} != $self->get_login_user($c)->{id}) {
-        my $res = $c->render_json({
-            error => 'forbidden',
-        });
-        $res->status(403);
-        return $res;
+        return $self->res_error($c, forbidden => 403);
     }
 
     my @recent_reservations;
@@ -180,8 +144,6 @@ get '/api/users/{id}' => [qw/login_required/] => sub {
         my $rows = $self->dbh->select_all('SELECT r.*, s.rank AS sheet_rank, s.num AS sheet_num FROM reservations r INNER JOIN sheets s ON s.id = r.sheet_id WHERE r.user_id = ? ORDER BY IFNULL(r.canceled_at, r.reserved_at) DESC LIMIT 5', $user->{id});
         for my $row (@$rows) {
             my $event = $self->get_event($row->{event_id});
-            $event->{public} = delete $event->{public_fg} ? JSON::XS::true : JSON::XS::false;
-            $event->{closed} = delete $event->{closed_fg} ? JSON::XS::true : JSON::XS::false;
             delete $event->{sheets};
             delete $event->{total};
             delete $event->{remains};
@@ -207,8 +169,6 @@ get '/api/users/{id}' => [qw/login_required/] => sub {
         my $rows = $self->dbh->select_all('SELECT DISTINCT event_id FROM reservations WHERE user_id = ? ORDER BY IFNULL(canceled_at, reserved_at) DESC LIMIT 5', $user->{id});
         for my $row (@$rows) {
             my $event = $self->get_event($row->{event_id});
-            $event->{public} = delete $event->{public_fg} ? JSON::XS::true : JSON::XS::false;
-            $event->{closed} = delete $event->{closed_fg} ? JSON::XS::true : JSON::XS::false;
             delete $event->{sheets}->{$_}->{detail} for keys %{ $event->{sheets} };
 
             push @recent_events => $event;
@@ -226,13 +186,7 @@ post '/api/actions/login' => [qw/allow_json_request/] => sub {
 
     my $user      = $self->dbh->select_row('SELECT * FROM users WHERE login_name = ?', $login_name);
     my $pass_hash = $self->dbh->select_one('SELECT SHA2(?, 256)', $password);
-    if (!$user || $pass_hash ne $user->{pass_hash}) {
-        my $res = $c->render_json({
-            error => 'authentication_failed',
-        });
-        $res->status(401);
-        return $res;
-    }
+    return $self->res_error($c, authentication_failed => 401) if !$user || $pass_hash ne $user->{pass_hash};
 
     my $session = Plack::Session->new($c->env);
     $session->set('user_id' => $user->{id});
@@ -258,27 +212,22 @@ get '/api/events/{id}' => sub {
     my ($self, $c) = @_;
     my $event_id = $c->args->{id};
 
-    my $user = $self->get_login_user($c);
+    my $user = $self->get_login_user($c) || {};
     my $event = $self->get_event($event_id, $user->{id});
-    if (!$event || !$event->{public_fg}) {
-        my $res = $c->render_json({
-            error => 'not_found',
-        });
-        $res->status(404);
-        return $res;
-    }
+    return $self->res_error($c, not_found => 404) if !$event || !$event->{public};
 
     $event = $self->sanitize_event($event);
     return $c->render_json($event);
 };
 
 sub get_events {
-    my $self = shift;
+    my ($self, $where) = @_;
+    $where ||= sub { $_->{public_fg} };
 
     my $txn = $self->dbh->txn_scope();
 
     my @events;
-    my @event_ids = map { $_->{id} } @{ $self->dbh->select_all('SELECT id FROM events WHERE public_fg = 1 ORDER BY id ASC') };
+    my @event_ids = map { $_->{id} } grep $where->($_), @{ $self->dbh->select_all('SELECT * FROM events ORDER BY id ASC') };
     for my $event_id (@event_ids) {
         my $event = $self->get_event($event_id);
 
@@ -312,7 +261,7 @@ sub get_event {
         $event->{total} += 1;
         $event->{sheets}->{$sheet->{rank}}->{total} += 1;
 
-        my $reservation = $self->dbh->select_row('SELECT * FROM reservations WHERE event_id = ? AND sheet_id = ? AND canceled_at IS NULL GROUP BY event_id, sheet_id HAVING reserved_at = MAX(reserved_at)', $event->{id}, $sheet->{id});
+        my $reservation = $self->dbh->select_row('SELECT * FROM reservations WHERE event_id = ? AND sheet_id = ? AND canceled_at IS NULL GROUP BY event_id, sheet_id HAVING reserved_at = MIN(reserved_at)', $event->{id}, $sheet->{id});
         if ($reservation) {
             $sheet->{mine}        = JSON::XS::true if $login_user_id && $reservation->{user_id} == $login_user_id;
             $sheet->{reserved}    = JSON::XS::true;
@@ -329,6 +278,9 @@ sub get_event {
         delete $sheet->{rank};
     }
 
+    $event->{public} = delete $event->{public_fg} ? JSON::XS::true : JSON::XS::false;
+    $event->{closed} = delete $event->{closed_fg} ? JSON::XS::true : JSON::XS::false;
+
     return $event;
 }
 
@@ -336,7 +288,8 @@ sub sanitize_event {
     my ($self, $event) = @_;
     my $sanitized = {%$event}; # shallow clone
     delete $sanitized->{price};
-    delete $sanitized->{public_fg};
+    delete $sanitized->{public};
+    delete $sanitized->{closed};
     return $sanitized;
 }
 
@@ -345,35 +298,16 @@ post '/api/events/{id}/actions/reserve' => [qw/allow_json_request login_required
     my $event_id = $c->args->{id};
     my $rank = $c->req->body_parameters->get('sheet_rank');
 
-    unless ($self->validate_rank($rank)) {
-        my $res = $c->render_json({
-            error => 'invalid_rank',
-        });
-        $res->status(400);
-        return $res;
-    }
-
     my $user  = $self->get_login_user($c);
     my $event = $self->get_event($event_id, $user->{id});
-    unless ($event && $event->{public_fg}) {
-        my $res = $c->render_json({
-            error => 'invalid_event',
-        });
-        $res->status(404);
-        return $res;
-    }
+    return $self->res_error($c, invalid_event => 404) unless $event && $event->{public};
+    return $self->res_error($c, invalid_rank => 400)  unless $self->validate_rank($rank);
 
     my $sheet;
     my $reservation_id;
     while (1) {
         $sheet = $self->dbh->select_row('SELECT * FROM sheets WHERE id NOT IN (SELECT sheet_id FROM reservations WHERE event_id = ? AND canceled_at IS NULL FOR UPDATE) AND `rank` = ? ORDER BY RAND() LIMIT 1', $event->{id}, $rank);
-        unless ($sheet) {
-            my $res = $c->render_json({
-                error => 'sold_out',
-            });
-            $res->status(409);
-            return $res;
-        }
+        return $self->res_error($c, sold_out => 409) unless $sheet;
 
         my $txn = $self->dbh->txn_scope();
         eval {
@@ -383,7 +317,7 @@ post '/api/events/{id}/actions/reserve' => [qw/allow_json_request login_required
         };
         if ($@) {
             $txn->rollback();
-            warn "re-try: rollbacked by $@";
+            warn "re-try: rollback by $@";
             next; # retry
         }
 
@@ -407,43 +341,23 @@ router ['DELETE'] => '/api/events/{id}/sheets/{rank}/{num}/reservation' => [qw/l
 
     my $user  = $self->get_login_user($c);
     my $event = $self->get_event($event_id, $user->{id});
-    unless ($event && $event->{public_fg}) {
-        my $res = $c->render_json({
-            error => 'invalid_event',
-        });
-        $res->status(404);
-        return $res;
-    }
-
-    unless ($self->validate_rank($rank)) {
-        my $res = $c->render_json({
-            error => 'invalid_rank',
-        });
-        $res->status(404);
-        return $res;
-    }
+    return $self->res_error($c, invalid_event => 404) unless $event && $event->{public};
+    return $self->res_error($c, invalid_rank => 404)  unless $self->validate_rank($rank);
 
     my $sheet = $self->dbh->select_row('SELECT * FROM sheets WHERE `rank` = ? AND num = ?', $rank, $num);
-    unless ($sheet) {
-        my $res = $c->render_json({
-            error => 'invalid_sheet',
-        });
-        $res->status(404);
-        return $res;
-    }
+    return $self->res_error($c, invalid_sheet => 404)  unless $sheet;
 
-    my $error;
-
+    my $res;
     my $txn = $self->dbh->txn_scope();
     eval {
-        my $reservation = $self->dbh->select_row('SELECT * FROM reservations WHERE event_id = ? AND sheet_id = ? AND canceled_at IS NULL GROUP BY event_id HAVING reserved_at = MAX(reserved_at) FOR UPDATE', $event->{id}, $sheet->{id});
+        my $reservation = $self->dbh->select_row('SELECT * FROM reservations WHERE event_id = ? AND sheet_id = ? AND canceled_at IS NULL GROUP BY event_id HAVING reserved_at = MIN(reserved_at) FOR UPDATE', $event->{id}, $sheet->{id});
         unless ($reservation) {
-            $error = 'not_reserved';
+            $res = $self->res_error($c, not_reserved => 400);
             $txn->rollback();
             return;
         }
         if ($reservation->{user_id} != $user->{id}) {
-            $error = 'not_permitted';
+            $res = $self->res_error($c, not_permitted => 403);
             $txn->rollback();
             return;
         }
@@ -452,30 +366,18 @@ router ['DELETE'] => '/api/events/{id}/sheets/{rank}/{num}/reservation' => [qw/l
         $txn->commit();
     };
     if ($@) {
-        $txn->rollback();
-        $error = 'unknown';
         warn "rollback by: $@";
+        $txn->rollback();
+        $res = $self->res_error($c);
     }
-
-    if ($error) {
-        my $res = $c->render_json({
-            error => $error,
-        });
-        $res->status(500);
-        $res->status(400) if $error eq 'not_reserved';
-        $res->status(403) if $error eq 'not_permitted';
-        return $res;
-    }
+    return $res if $res;
 
     return $c->req->new_response(204, [], '');
 };
 
 sub validate_rank {
     my ($self, $rank) = @_;
-    return unless $rank;
-
-    my $valid = $self->dbh->select_one('SELECT COUNT(*) FROM sheets WHERE `rank` = ?', $rank);
-    return $valid;
+    return $self->dbh->select_one('SELECT COUNT(*) FROM sheets WHERE `rank` = ?', $rank);
 }
 
 filter admin_login_required => sub {
@@ -485,13 +387,7 @@ filter admin_login_required => sub {
         my $session = Plack::Session->new($c->env);
 
         my $administrator = $self->get_login_administrator($c);
-        unless ($administrator) {
-            my $res = $c->render_json({
-                error => 'admin_login_required',
-            });
-            $res->status(401);
-            return $res;
-        }
+        return $self->res_error($c, admin_login_required => 401) unless $administrator;
 
         $app->($self, $c);
     };
@@ -501,12 +397,9 @@ filter fillin_administrator => sub {
     my $app = shift;
     return sub {
         my ($self, $c) = @_;
-        my $session = Plack::Session->new($c->env);
 
-        if (my $administrator_id = $session->get('administrator_id')) {
-            my $administrator = $self->get_administrator($administrator_id);
-            $c->stash->{administrator} = $administrator;
-        }
+        my $administrator = $self->get_login_administrator($c);
+        $c->stash->{administrator} = $administrator if $administrator;
 
         $app->($self, $c);
     };
@@ -516,16 +409,7 @@ get '/admin/' => [qw/fillin_administrator/] => sub {
     my ($self, $c) = @_;
 
     my @events;
-    if ($c->stash->{administrator}) {
-        my @event_ids = map { $_->{id} } @{ $self->dbh->select_all('SELECT id FROM events') };
-        for my $event_id (@event_ids) {
-            my $event = $self->get_event($event_id);
-            delete $event->{sheets}->{$_}->{detail} for keys %{ $event->{sheets} };
-            $event->{public} = delete $event->{public_fg} ? JSON::XS::true : JSON::XS::false;
-            $event->{closed} = delete $event->{closed_fg} ? JSON::XS::true : JSON::XS::false;
-            push @events => $event;
-        }
-    }
+    @events = $self->get_events(sub { $_ }) if $c->stash->{administrator};
 
     return $c->render('admin.tx', {
         events      => \@events,
@@ -540,13 +424,7 @@ post '/admin/api/actions/login' => [qw/allow_json_request/] => sub {
 
     my $administrator = $self->dbh->select_row('SELECT * FROM administrators WHERE login_name = ?', $login_name);
     my $pass_hash     = $self->dbh->select_one('SELECT SHA2(?, 256)', $password);
-    if (!$administrator || $pass_hash ne $administrator->{pass_hash}) {
-        my $res = $c->render_json({
-            error => 'authentication_failed',
-        });
-        $res->status(401);
-        return $res;
-    }
+    return $self->res_error($c, authentication_failed => 401) if !$administrator || $pass_hash ne $administrator->{pass_hash};
 
     my $session = Plack::Session->new($c->env);
     $session->set('administrator_id' => $administrator->{id});
@@ -562,36 +440,18 @@ post '/admin/api/actions/logout' => [qw/admin_login_required/] => sub {
     return $c->req->new_response(204, [], '');
 };
 
-sub get_administrator {
-    my ($self, $administrator_id) = @_;
-    my $administrator = $self->dbh->select_row('SELECT * FROM administrators WHERE id = ?', $administrator_id);
-    delete $administrator->{login_name};
-    delete $administrator->{pass_hash};
-    return $administrator;
-}
-
 sub get_login_administrator {
     my ($self, $c) = @_;
-
     my $session = Plack::Session->new($c->env);
     my $administrator_id = $session->get('administrator_id');
     return unless $administrator_id;
-    return $self->get_administrator($administrator_id);
+    return $self->dbh->select_row('SELECT id, nickname FROM administrators WHERE id = ?', $administrator_id);
 }
 
 get '/admin/api/events' => [qw/admin_login_required/] => sub {
     my ($self, $c) = @_;
 
-    my @events;
-    my @event_ids = map { $_->{id} } @{ $self->dbh->select_all('SELECT id FROM events') };
-    for my $event_id (@event_ids) {
-        my $event = $self->get_event($event_id);
-        delete $event->{sheets}->{$_}->{detail} for keys %{ $event->{sheets} };
-        $event->{public} = delete $event->{public_fg} ? JSON::XS::true : JSON::XS::false;
-        $event->{closed} = delete $event->{closed_fg} ? JSON::XS::true : JSON::XS::false;
-        push @events => $event;
-    }
-
+    my @events = $self->get_events(sub { $_ });
     return $c->render_json(\@events);
 };
 
@@ -614,8 +474,6 @@ post '/admin/api/events' => [qw/allow_json_request admin_login_required/] => sub
     }
 
     my $event = $self->get_event($event_id);
-    $event->{public} = delete $event->{public_fg} ? JSON::XS::true : JSON::XS::false;
-    $event->{closed} = delete $event->{closed_fg} ? JSON::XS::true : JSON::XS::false;
     return $c->render_json($event);
 };
 
@@ -624,16 +482,8 @@ get '/admin/api/events/{id}' => [qw/admin_login_required/] => sub {
     my $event_id = $c->args->{id};
 
     my $event = $self->get_event($event_id);
-    unless ($event) {
-        my $res = $c->render_json({
-            error => 'not_found',
-        });
-        $res->status(404);
-        return $res;
-    }
+    return $self->res_error($c, not_found => 404) unless $event;
 
-    $event->{public} = delete $event->{public_fg} ? JSON::XS::true : JSON::XS::false;
-    $event->{closed} = delete $event->{closed_fg} ? JSON::XS::true : JSON::XS::false;
     return $c->render_json($event);
 };
 
@@ -645,26 +495,12 @@ post '/admin/api/events/{id}/actions/edit' => [qw/allow_json_request admin_login
     $public = 0 if $closed;
 
     my $event = $self->get_event($event_id);
-    unless ($event) {
-        my $res = $c->render_json({
-            error => 'not_found',
-        });
-        $res->status(404);
-        return $res;
-    }
+    return $self->res_error($c, not_found => 404) unless $event;
 
-    if ($event->{closed_fg}) {
-        my $res = $c->render_json({
-            error => 'cannot_edit_closed_event',
-        });
-        $res->status(400);
-        return $res;
-    } elsif ($event->{public_fg} && $closed) {
-        my $res = $c->render_json({
-            error => 'cannot_close_public_event',
-        });
-        $res->status(400);
-        return $res;
+    if ($event->{closed}) {
+        return $self->res_error($c, cannot_edit_closed_event => 400);
+    } elsif ($event->{public} && $closed) {
+        return $self->res_error($c, cannot_close_public_event => 400);
     }
 
     my $txn = $self->dbh->txn_scope();
@@ -677,8 +513,6 @@ post '/admin/api/events/{id}/actions/edit' => [qw/allow_json_request admin_login
     }
 
     $event = $self->get_event($event_id);
-    $event->{public} = delete $event->{public_fg} ? JSON::XS::true : JSON::XS::false;
-    $event->{closed} = delete $event->{closed_fg} ? JSON::XS::true : JSON::XS::false;
     return $c->render_json($event);
 };
 
@@ -748,6 +582,16 @@ sub render_report_csv {
         'Content-Type'        => 'text/csv; charset=UTF-8',
         'Content-Disposition' => 'attachment; filename="report.csv"',
     ], $body);
+    return $res;
+}
+
+sub res_error {
+    my ($self, $c, $error, $status) = @_;
+    $error  ||= 'unknown';
+    $status ||= 500;
+
+    my $res = $c->render_json({ error => $error });
+    $res->status($status);
     return $res;
 }
 
