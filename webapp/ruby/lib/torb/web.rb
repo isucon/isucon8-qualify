f17fb3e8bfa74269afb9ba4bb7501a803da680b0
abicky <takeshi.arabiki@gmail.com>
2018-09-02 22:04:51 +0900

    Implement "POST /api/events/:id/actions/reserve"

--- webapp/ruby/lib/torb/web.rb
+++ webapp/ruby/lib/torb/web.rb
@@ -114,6 +114,10 @@ module Torb
         db.xquery('SELECT id, nickname FROM users WHERE id = ?', user_id).first
       end
 
+      def validate_rank(rank)
+        db.xquery('SELECT COUNT(*) FROM sheets WHERE `rank` = ?', rank).first['COUNT(*)'] > 0
+      end
+
       def body_params
         @body_params ||= JSON.parse(request.body.tap(&:rewind).read)
       end
@@ -186,6 +190,37 @@ module Torb
 
       event = sanitize_event(event)
       event.to_json
+    end
+
+    post '/api/events/:id/actions/reserve', login_required: true do |event_id|
+      rank = body_params['sheet_rank']
+
+      user  = get_login_user
+      event = get_event(event_id, user['id'])
+      halt_with_error 404, 'invalid_event' unless event && event['public']
+      halt_with_error 400, 'invalid_rank' unless validate_rank(rank)
+
+      sheet = nil
+      reservation_id = nil
+      loop do
+        sheet = db.xquery('SELECT * FROM sheets WHERE id NOT IN (SELECT sheet_id FROM reservations WHERE event_id = ? AND canceled_at IS NULL FOR UPDATE) AND `rank` = ? ORDER BY RAND() LIMIT 1', event['id'], rank).first
+        halt_with_error 409, 'sold_out' unless sheet
+        db.query('BEGIN')
+        begin
+          db.xquery('INSERT INTO reservations (event_id, sheet_id, user_id, reserved_at) VALUES (?, ?, ?, ?)', event['id'], sheet['id'], user['id'], Time.now.utc.strftime('%F %T.%6N'))
+          reservation_id = db.last_id
+          db.query('COMMIT')
+        rescue => e
+          db.query('ROLLBACK')
+          warn "re-try: rollback by #{e}"
+          next
+        end
+
+        break
+      end
+
+      status 202
+      { reservation_id: reservation_id, sheet_rank: rank, sheet_num: sheet['num'] } .to_json
     end
   end
 end
