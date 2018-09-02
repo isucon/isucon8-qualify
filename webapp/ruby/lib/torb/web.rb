1bba96c642e3c8f7deb441384a336a560098019d
abicky <takeshi.arabiki@gmail.com>
2018-09-02 18:46:45 +0900

    Implement "GET /"

--- webapp/ruby/lib/torb/web.rb
+++ webapp/ruby/lib/torb/web.rb
@@ -1,4 +1,8 @@
+require 'json'
 require 'sinatra/base'
+require 'erubi'
+require 'mysql2'
+require 'mysql2-cs-bind'
 
 module Torb
   class Web < Sinatra::Base
@@ -12,8 +16,97 @@ module Torb
     set :sessions, key: 'torb_session', expire_after: 3600
     set :session_secret, 'tagomoris'
 
+    set :erb, escape_html: true
+
+    helpers do
+      def db
+        Thread.current[:db] ||= Mysql2::Client.new(
+          host: ENV['DB_HOST'],
+          port: ENV['DB_PORT'],
+          username: ENV['DB_USER'],
+          password: ENV['DB_PASS'],
+          database: ENV['DB_DATABASE'],
+          database_timezone: :utc,
+          reconnect: true,
+          init_command: 'SET SESSION sql_mode="STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION"',
+        )
+      end
+
+      def get_events(where = nil)
+        where ||= ->(e) { e['public_fg'] == 1 }
+
+        db.query('BEGIN')
+        event_ids = db.query('SELECT * FROM events ORDER BY id ASC').select(&where).map { |e| e['id'] }
+        events = event_ids.map do |event_id|
+          event = get_event(event_id)
+          event['sheets'].each { |sheet| sheet.delete('detail') }
+          event
+        end
+        db.query('COMMIT')
+
+        events
+      end
+
+      def get_event(event_id, login_user_id = nil)
+        event = db.xquery('SELECT * FROM events WHERE id = ?', event_id).first
+        return unless event
+
+        # zero fill
+        event['total']   = 0
+        event['remains'] = 0
+        event['sheets'] = {}
+        %w[S A B C].each do |rank|
+          event['sheets'][rank] = { 'total' => 0, 'remains' => 0, 'detail' => [] }
+        end
+
+        sheets = db.query('SELECT * FROM sheets ORDER BY `rank`, num')
+        sheets.each do |sheet|
+          event['sheets'][sheet['rank']]['price'] ||= event['price'] + sheet['price']
+          event['total'] += 1
+          event['sheets'][sheet['rank']]['total'] += 1
+
+          reservation = db.xquery('SELECT * FROM reservations WHERE event_id = ? AND sheet_id = ? AND canceled_at IS NULL GROUP BY event_id, sheet_id HAVING reserved_at = MIN(reserved_at)', event['id'], sheet['id']).first
+          if reservation
+            sheet['mine']        = true if login_user_id && reservation['user_id'] == login_user_id
+            sheet['reserved']    = true
+            sheet['reserved_at'] = reservation['reserved_at'].to_i
+          else
+            event['remains'] += 1
+            event['sheets'][sheet['rank']]['remains'] += 1
+          end
+
+          event['sheets'][sheet['rank']]['detail'].push(sheet)
+
+          sheet.delete('id')
+          sheet.delete('price')
+          sheet.delete('rank')
+        end
+
+        event['public'] = (event.delete('public_fg') == 1)
+        event['closed'] = (event.delete('closed_fg') == 1)
+
+        event
+      end
+
+      def sanitize_event(event)
+        sanitized = event.dup  # shallow clone
+        sanitized.delete('price')
+        sanitized.delete('public')
+        sanitized.delete('closed')
+        sanitized
+      end
+
+      def get_login_user
+        user_id = session[:user_id]
+        return unless user_id
+        db.xquery('SELECT id, nickname FROM users WHERE id = ?', user_id).first
+      end
+    end
+
     get '/' do
-      'Hello torb.ruby'
+      @user   = get_login_user
+      @events = get_events.map(&method(:sanitize_event))
+      erb :index
     end
   end
 end
