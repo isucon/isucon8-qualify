59ed518cf20b904368daa49f00c350ab94e70460
abicky <takeshi.arabiki@gmail.com>
2018-09-03 03:28:05 +0900

    Add a symlink to "static" directory

--- webapp/ruby/lib/torb/web.rb
+++ webapp/ruby/lib/torb/web.rb
@@ -12,7 +12,6 @@ module Torb
     end
 
     set :root, File.expand_path('../..', __dir__)
-    set :public_folder, File.expand_path('../static', root)
     set :sessions, key: 'torb_session', expire_after: 3600
     set :session_secret, 'tagomoris'
     set :protection, frame_options: :deny
