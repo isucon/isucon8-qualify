8269a7f84efd93c8de6ef792b1d0003cbcf8216a
xaicron <xaicron@gmail.com>
2018-09-12 06:24:07 +0900

    cleanup

--- portal/lib/ISUCON8/Portal/Model/Admin.pm
+++ portal/lib/ISUCON8/Portal/Model/Admin.pm
@@ -30,6 +30,9 @@ sub find_user {
                     name     => $name,
                     password => $password,
                 },
+                {
+                    limit => 1,
+                },
             );
             $user = $dbh->selectrow_hashref($stmt, undef, @bind);
         });
@@ -44,49 +47,6 @@ sub find_user {
     }
 
     return $user;
-}
-
-1;
-__END__
-
-sub create_account {
-    my ($self, $params) = @_;
-    my $account_name  = $params->{account_name};
-    my $user_group_id = $params->{user_group_id};
-    my $company       = $params->{company};
-    my $daily_budget  = $params->{daily_budget};
-
-    my $account_id;
-    eval {
-        $account_id = $self->db->seq('SEQ_W')->nextval('ads_accounts');
-        $self->db->connect('ADS_W')->txn(sub {
-            my $dbh = shift;
-            my ($stmt, @bind) = $self->sql->insert(
-                'ads_accounts',
-                {
-                    id              => $account_id,
-                    user_group_id   => $user_group_id,
-                    account_name    => encode_utf8($account_name),
-                    company         => encode_utf8($company),
-                    daily_budget    => $daily_budget,
-                    publish_status  => ADS_PUBLISH_STATUS_PAUSED,
-                    created_at      => \'UNIX_TIMESTAMP()',
-                    updated_at      => \'UNIX_TIMESTAMP()',
-                },
-            );
-            $dbh->do($stmt, undef, @bind);
-        });
-    };
-    if (my $e = $@) {
-        $e->rethrow if ref $e eq 'MoonShot::AdsOPE::Exception';
-        MoonShot::AdsOPE::Exception->throw(
-            code    => ERROR_INTERNAL_ERROR,
-            message => "$e",
-            logger  => sub { $self->log->critf(@_) },
-        );
-    }
-
-    return $account_id;
 }
 
 1;
