7dbbc4fa4c01e50540b8204c289364241318f601
NomotoEriko <nomoto.eriko@ist.osaka-u.ac.jp>
2019-04-26 01:27:38 +0900

    :bug: bug fix https://github.com/OnizukaLab/isucon-study/issues/7

--- webapp/python/run_app.sh
+++ webapp/python/run_app.sh
@@ -8,5 +8,4 @@ mysql -uroot -e"GRANT ALL on torb.* TO isucon@'%';"
 mysql -uroot -e"CREATE USER isucon@'localhost' IDENTIFIED BY 'isucon';"
 mysql -uroot -e"GRANT ALL on torb.* TO isucon@'localhost';"
 ${ROOT}/db/init.sh
-${ROOT}/db/init-user.sh
 DB_DATABASE=torb DB_HOST=127.0.0.1 DB_PORT=3306 DB_USER=isucon DB_PASS=isucon python ${APP}/app.py
